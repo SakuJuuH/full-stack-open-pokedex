73190cb5077a4238f0f4be20b0cfcc6905e30d9b
SakuJuuH <sakuhei@metropolia.fi>
2025-07-26 01:57:11 +0300

    style: fix linting errors

--- e2e-tests/example.spec.js
+++ e2e-tests/example.spec.js
@@ -1,20 +1,20 @@
-const { test, describe, expect, beforeEach } = require("@playwright/test");
+const { test, describe, expect } = require('@playwright/test')
 
-describe("Pokedex", () => {
-	test("front page can be opened", async ({ page }) => {
-		await page.goto("http://localhost:8081");
-		await expect(page.getByText("ivysaur")).toBeVisible();
-		await expect(
-			page.getByText(
-				"Pokémon and Pokémon character names are trademarks of Nintendo."
-			)
-		).toBeVisible();
-	});
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('http://localhost:8081')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
 
-	test("navigation to particular pokemon works", async ({ page }) => {
-		await page.goto("http://localhost:8081");
-		await page.getByRole("link", { name: "ivysaur" }).click();
-		await expect(page.getByText("ivysaur")).toBeVisible();
-		await expect(page.getByText("chlorophyll")).toBeVisible();
-	});
-});
+  test('navigation to particular pokemon works', async ({ page }) => {
+    await page.goto('http://localhost:8081')
+    await page.getByRole('link', { name: 'ivysaur' }).click()
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
