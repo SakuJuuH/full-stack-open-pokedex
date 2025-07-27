3255793b9975da2692dc042acdb9d9ad492d800e
SakuJuuH <sakuhei@metropolia.fi>
2025-07-27 23:11:43 +0300

    CI/CD: simulate to see if actions catches bug

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
+
+app.get("/health", (req, res) => {
+	if (true) throw new Error("Health check failed");
+	res.send("OK");
+});
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+	console.log(`server started on port ${PORT}`);
+});
