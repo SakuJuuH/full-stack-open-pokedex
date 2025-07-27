1d5dffad0475a47f5fe2381a284318defe60b642
SakuJuuH <sakuhei@metropolia.fi>
2025-07-28 00:01:21 +0300

    refactor: add comment for healthcheck endpoint

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
+  // simple health check endpoint
+  // this can be used to check if the server is running
+  console.log('Health check received')
   res.status(200).send('ok')
 })
 
