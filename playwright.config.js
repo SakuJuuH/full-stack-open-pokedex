40decdce9f03b1c946030c9326c4d045c337aa21
SakuJuuH <sakuhei@metropolia.fi>
2025-07-27 15:50:41 +0300

    style: run eslint fix and update pipeline to not use eslint:fix

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { defineConfig, devices } = require("@playwright/test");
+const { defineConfig, devices } = require('@playwright/test')
 
 /**
  * Read environment variables from file.
@@ -13,71 +13,71 @@ const { defineConfig, devices } = require("@playwright/test");
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-	testDir: "./e2e-tests",
-	/* Run tests in files in parallel */
-	fullyParallel: false,
-	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	forbidOnly: !!process.env.CI,
-	/* Retry on CI only */
-	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
-	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: "html",
-	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-	webServer: {
-		command: process.env.CI ? "npm run start-ci" : "npm start",
-		port: process.env.CI ? 8081 : 8080,
-		timeout: 120 * 1000, // 2 minutes
-		reuseExistingServer: !process.env.CI,
-	},
-	use: {
-		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: process.env.CI
-			? "http://localhost:8081"
-			: "http://localhost:8080",
+  testDir: './e2e-tests',
+  /* Run tests in files in parallel */
+  fullyParallel: false,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  webServer: {
+    command: process.env.CI ? 'npm run start-ci' : 'npm start',
+    port: process.env.CI ? 8081 : 8080,
+    timeout: 120 * 1000, // 2 minutes
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    baseURL: process.env.CI
+      ? 'http://localhost:8081'
+      : 'http://localhost:8080',
 
-		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: "on-first-retry",
-	},
+    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    trace: 'on-first-retry',
+  },
 
-	/* Configure projects for major browsers */
-	projects: [
-		{
-			name: "chromium",
-			use: { ...devices["Desktop Chrome"] },
-		},
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
 
-		{
-			name: "firefox",
-			use: { ...devices["Desktop Firefox"] },
-		},
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
 
-		{
-			name: "webkit",
-			use: { ...devices["Desktop Safari"] },
-		},
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
 
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
+    /* Test against mobile viewports. */
+    // {
+    //   name: 'Mobile Chrome',
+    //   use: { ...devices['Pixel 5'] },
+    // },
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] },
+    // },
 
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
-	],
+    /* Test against branded browsers. */
+    // {
+    //   name: 'Microsoft Edge',
+    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+    // },
+    // {
+    //   name: 'Google Chrome',
+    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    // },
+  ],
 
-	/* Run your local dev server before starting the tests */
-});
+  /* Run your local dev server before starting the tests */
+})
