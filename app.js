8d46d256d86201dd75d69a1904704d9699bf2a3f
SakuJuuH <sakuhei@metropolia.fi>
2025-07-28 02:32:35 +0300

    test: check if discord notify works properly

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const PORT = process.env.PORT || 3000
 app.use(express.static('dist'))
 
 app.get('/health', (req, res) => {
-  // simple health check endpoint
-  // this can be used to check if the server is running
   console.log('Health check received')
   res.status(200).send('ok')
 })
