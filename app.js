c22d6df44cb5896ae8cd641339b79e1be37833a8
SakuJuuH <sakuhei@metropolia.fi>
2025-07-30 22:08:38 +0300

    fix: comment out failed health check endpoint and disable schedule for health check workflow as it works

--- app.js
+++ app.js
@@ -10,12 +10,15 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Health check passed' })
 })
 
+/* This endpoint simulates a health check failure.
+    It can be used to test the health check failure handling in the CI/CD pipeline.
 app.get('/failed-health', (req, res) => {
   res.status(500).json({
     status: 'error',
     message: 'Simulated health check failure',
   })
 })
+*/
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
